63add580eff57562b2fdc2a4f91fa0b62721e4a0
yanc <volcas@gmail.com>
2020-11-13 16:55:50 +0800

    not work yet

--- src/main.ino
+++ src/main.ino
@@ -1,23 +1,43 @@
 #include <Wire.h>
+#include "driver/adc.h"
 
 const int PIN_CLK   = 17;
 const int PIN_SOIL  = 9;
+
+#define NO_OF_SAMPLES   64          //Multisampling
+
+static const adc_channel_t channel = ADC_CHANNEL_8;     // GPIO7 if ADC1, GPIO17 if ADC2
+static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
+static const adc_atten_t atten = ADC_ATTEN_DB_2_5;
+static const adc_unit_t unit = ADC_UNIT_1;
 
 void setup() {
     Serial.begin(115200);
     delay(10);
 
     ledcAttachPin(PIN_CLK, 1);
-    ledcAttachPin(PIN_SOIL, 2);
-    ledcSetup(1, 12000, 8);
-    ledcWrite(PIN_CLK, 128);
+    //ledcAttachPin(PIN_SOIL, 2);
+    ledcSetup(1, 8000000, 3);
+    ledcWrite(PIN_CLK, 4);
     delay(50);
+
+    //Configure ADC
+    adc1_config_width(width);
+    adc1_config_channel_atten((adc1_channel_t)channel, atten);
 }
 
 void loop() {
-    ledcWrite(PIN_CLK, 128);
     delay(50);
-    float rawVal = ledcRead(2);
-    Serial.printf("loop %f\n", rawVal);
+    //float rawVal = analogRead(PIN_SOIL);
+
+    uint32_t adc_reading = 0;
+    //Multisampling
+    for (int i = 0; i < NO_OF_SAMPLES; i++) {
+        adc_reading += adc1_get_raw((adc1_channel_t)channel);
+    }
+    adc_reading /= NO_OF_SAMPLES;
+
+    Serial.printf("loop %d\n", adc_reading);
     delay(1000);
+
 }
